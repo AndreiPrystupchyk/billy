7bf2b65363ffa66ffa4f99afc835866ebde1daa3
AndreiPrystupchyk <andrei.prystupchyk@gmail.com>
2023-07-23 11:41:53 +0200

    Add files via upload

--- modules/steam.py
+++ modules/steam.py
@@ -1,17 +1,40 @@
 import requests, json, config
 import cache
-
-status = {0:'Offline', 1:'Online', 2:'Busy', 3:'Away', 4:'Snooze', 5:'looking to trade', 6:'looking to play'}
+from bs4 import BeautifulSoup
+from functools import reduce
+from time import sleep
 
 
 def steamWhoFromSpermobakiOnlineRequest(bot,messageChatId):
-    url = f'https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key={config.steamApiKey}&format=json&steamids={list(member["steamId"] for member in cache.steamList)}'
-    response = json.loads(requests.get(url).text)
+    listOfPlayersID = reduce((lambda x, y: x+y) , list(member["steamId"] for member in cache.steamList))
     answer = ''
-    for i in range(len(response['response']['players'])):
-        if response["response"]["players"][i]["personastate"] != 0:
-            answer += f'\n{response["response"]["players"][i]["personaname"]}: {response["response"]["players"][i].get("gameextrainfo",status[response["response"]["players"][i]["personastate"]])}'
-    if not answer == '':
-        bot.send_message(messageChatId,answer)
-    else:
-        bot.send_message(messageChatId,'Все спермачи offline :(')
+    for i in range(len(listOfPlayersID)):
+        sleep(0.1)
+        pageRequest = requests.get(f"https://steamcommunity.com/miniprofile/{int(listOfPlayersID[i]) - 76561197960265728}")
+        if pageRequest.status_code != 200:
+            print(f"status code {pageRequest.status_code} returned whilst trying to fetch the enhanced rich presence info for steam user ID {i}, ignoring function")
+
+        soup = BeautifulSoup(pageRequest.content, "html.parser")
+        isPersonaInGame = soup.find_all("span", {"class": "persona in-game"})
+        isPersonaOnline = soup.find_all("span", {"class": "persona online"})
+        if isPersonaInGame or isPersonaOnline:
+            playerName = ''
+            miniGameName = ''
+            gameRichPresence = ''
+            rich_presence = ''
+
+            if isPersonaInGame:
+                playerName = soup.find("span", class_="persona in-game").contents[0]
+                miniGameName = soup.find("span", class_="miniprofile_game_name").contents[0]
+                rich_presence = soup.find("span", class_="rich_presence")
+                if rich_presence != None:
+                    gameRichPresence = soup.find("span", class_="rich_presence").contents[0]
+                answer += f'\n{playerName}: {miniGameName} {gameRichPresence}'
+
+
+            if isPersonaOnline:
+                playerName = soup.find("span", class_="persona online").contents[0]
+                answer += f'\n{playerName}: online'
+    if answer == '':
+        answer = 'Все спермачи offline :('
+    bot.send_message(messageChatId, answer)
